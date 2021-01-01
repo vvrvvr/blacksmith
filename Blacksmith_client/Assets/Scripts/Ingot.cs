9d653bb3559112ab1018d3d0effd5a2cc6d78014
Widu <alitanar@mail.ru>
2021-01-01 17:42:26 +0300

    !R split ingot generate and dragging logic between two scripts

--- Blacksmith_client/Assets/Scripts/Ingot.cs
+++ Blacksmith_client/Assets/Scripts/Ingot.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
-using UnityEngine.EventSystems;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider))]
 public class Ingot : MonoBehaviour
 {
     [Header("Ingot Size")]
@@ -13,26 +11,8 @@ public class Ingot : MonoBehaviour
     [Header("Prefabs")]
     [SerializeField] private Cube cubePrefab;
 
-    [Header("Settings")]
-    [SerializeField] private Camera mainCamera;
-    [SerializeField] private Transform colliderPlane;
-    [SerializeField] private LayerMask colliderPlaneLayer;
-    [SerializeField] private LayerMask ingotLayer;
-
-    private List<Cube> cubes = new List<Cube>();
     private BoxCollider boxCollider;
-
-    #region DoubleClick
-    private const float DoubleClickSpeed = 0.25f;
-    private float doubleClickTime = 0f;
-    #endregion
-
-    #region Dragging
-    private Vector3 lastDragPos = Vector3.zero;
-    private Vector3 currentPoint;
-    private float dragDistance = 0f;
-    #endregion
-
+    private List<Cube> cubes = new List<Cube>();
 
     private void Start()
     {
@@ -40,65 +20,6 @@ public class Ingot : MonoBehaviour
         boxCollider.size = new Vector3(width, height, length);
         SpawnCubes();
         boxCollider.center = new Vector3((width - 1) * 0.5f, (height - 1) * 0.5f, (length - 1) * 0.5f);
-    }
-
-    private void Update()
-    {
-        if (doubleClickTime < 1f)
-            doubleClickTime += Time.deltaTime;
-        if(Input.GetMouseButtonDown(0))
-        {
-            if (doubleClickTime <= DoubleClickSpeed)
-            {
-                InitCubes();
-                enabled = false;
-                boxCollider.enabled = false;
-            }
-            doubleClickTime = 0f;
-        }
-    }
-
-    private void OnMouseDown()
-    {
-        if (!enabled) return;
-        
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, ingotLayer))
-        {
-            lastDragPos = hit.point;
-            colliderPlane.position = hit.point;
-            colliderPlane.gameObject.SetActive(true);
-        }
-    }
-
-    private void OnMouseDrag()
-    {
-        if (!enabled) return;
-
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, colliderPlaneLayer))
-        {
-            currentPoint = hit.point;
-            Vector3 direction = currentPoint - lastDragPos;
-            dragDistance += direction.magnitude;
-            if (dragDistance >= 1f)
-            {
-                dragDistance = 0f;
-                Vector3 projection1 = Vector3.Project(direction, Vector3.forward);
-                Vector3 projection2 = Vector3.Project(direction, Vector3.right);
-                if(projection1.magnitude > projection2.magnitude)
-                    transform.position += projection1.normalized;
-                else
-                    transform.position += projection2.normalized;
-            }
-            lastDragPos = currentPoint;
-        }
-    }
-
-    private void OnMouseUp()
-    {
-        if (!enabled) return;
-        colliderPlane.gameObject.SetActive(false);
     }
 
     private void SpawnCubes()
@@ -116,9 +37,10 @@ public class Ingot : MonoBehaviour
         }
     }
 
-    private void InitCubes()
+    public void InitCubes()
     {
-        foreach(Cube cube in cubes)
+        boxCollider.enabled = false;
+        foreach (Cube cube in cubes)
         {
             cube.Init();
         }
---
+++ Blacksmith_client/Assets/Scripts/IngotDragAndDrop.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+public class IngotDragAndDrop : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Ingot ingot;
+    [SerializeField] private Transform colliderPlane;
+    [SerializeField] private LayerMask colliderPlaneLayer;
+    [SerializeField] private LayerMask ingotLayer;
+
+    #region DoubleClick
+    private const float DoubleClickSpeed = 0.25f;
+    private float doubleClickTime = 0f;
+    #endregion
+
+    #region Dragging
+    private Vector3 lastDragPos = Vector3.zero;
+    private Vector3 currentPoint;
+    private float dragDistance = 0f;
+    #endregion
+
+    private void Update()
+    {
+        if (doubleClickTime < 1f)
+            doubleClickTime += Time.deltaTime;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (doubleClickTime <= DoubleClickSpeed)
+            {
+                ingot.InitCubes();
+                enabled = false;
+            }
+            doubleClickTime = 0f;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!enabled) return;
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, ingotLayer))
+        {
+            lastDragPos = hit.point;
+            colliderPlane.position = hit.point;
+            colliderPlane.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!enabled) return;
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, colliderPlaneLayer))
+        {
+            currentPoint = hit.point;
+            Vector3 direction = currentPoint - lastDragPos;
+            dragDistance += direction.magnitude;
+            if (dragDistance >= 1f)
+            {
+                dragDistance = 0f;
+                Vector3 projection1 = Vector3.Project(direction, Vector3.forward);
+                Vector3 projection2 = Vector3.Project(direction, Vector3.right);
+                if (projection1.magnitude > projection2.magnitude)
+                    transform.position += projection1.normalized;
+                else
+                    transform.position += projection2.normalized;
+            }
+            lastDragPos = currentPoint;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (!enabled) return;
+        colliderPlane.gameObject.SetActive(false);
+    }
+
+
+}
